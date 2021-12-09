d14f6c94a335835c502a78727c31ba3878c2f2c2
Luis Chavez <Dkangelas@gmail.com>
2021-12-09 14:19:14 -0600

    impr-css/js

---
+++ assets/java.js
@@ -0,0 +1,23 @@
+
+
+
+
+function startquiz() {
+  document.getElementById("what").innerHTML = "";
+  document.getElementById("H1").innerHTML = "";
+  document.getElementById("btn").innerHTML = "";
+  document.getElementById("question1").innerHTML = quizQuestions;
+
+}
+
+
+
+
+
+
+
+
+document.getElementById("btn").addEventListener("click", startquiz);
+
+
+ 
